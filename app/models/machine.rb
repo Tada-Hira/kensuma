3a48afb52ce188fb46c9c6554add0b689d2d6ddb
akito sato <eight2019@outlook.jp>
2024-04-07 21:26:36 +0900

    不具合項目321修正

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -10,8 +10,18 @@ class Machine < ApplicationRecord
   validates :control_number, presence: true
   validates :inspector, presence: true
   validates :handler, presence: true
+  before_validation :convert_to_full_width_katakana # 機械名に対して半角で入力しても全角に変換する
 
   def to_param
     uuid
+  end
+
+  private
+
+  # 半角カタカナを全角カタカナに変換する
+  def convert_to_full_width_katakana
+    if name.present?
+      self.name = name.gsub(/[\uFF61-\uFF9F]+/) { |str| str.unicode_normalize(:nfkc) }
+    end
   end
 end
